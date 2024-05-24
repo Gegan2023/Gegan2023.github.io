5f8349ce4b2f20450de92dd3a423307277cb4a9f
Gerry <145372093+Gegan2023@users.noreply.github.com>
2024-05-24 03:38:34 +0000

    added lots of javascript for main.js this took a lot of reaserch but using dev tools and MDN I think i got all the required things down

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,32 @@
 console.log("task one");
-1 + 1
+
+for (let i = 1; i <= 10; i++) {
+    console.log(i * i);
+}
+
+for (let i = 5; i >= 1; i--) {
+    console.log(i);
+}
+console.log("Blastoff!");
+
+for (let i = 2; i <= 50; i += 2) {
+    console.log(i);
+}
+
+let sum = 0;
+for (let i = 1; i <= 100; i++) {
+    sum += i;
+}
+console.log("The sum of numbers from 1 to 100 is:", sum);
+
+function factorial(n) {
+    let result = 1;
+    for (let i = 1; i <= n; i++) {
+        result *= i;
+    }
+    return result;
+}
+
+for (let i = 1; i <= 10; i++) {
+    console.log(i + "! = " + factorial(i));
+}
